2a43e2b72ceb13ed037d28e1101a23799deea25c
FOZLAUR <fozla@oocl.com>
2020-10-15 20:49:17 +0800

    [mars-rover-tdd][LaurenceFoz] test: Added test case for (0,0) S L; No changes to logic

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -81,4 +81,19 @@ class DemoTest {
         assertEquals(0,marsRover.getLocationY());
         assertEquals("W",marsRover.getHeading());
     }
+    
+    @Test
+    void should_return_x_0_y_0_heading_E_when_change_heading_left_given_x_0_y_0_heading_S() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"S");
+        List<String> commands = new CommandSplitter("L").getCommandList();
+
+        //when
+        marsRover.executeBatchCommand(commands);
+
+        //should
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(0,marsRover.getLocationY());
+        assertEquals("E",marsRover.getHeading());
+    }
 }
