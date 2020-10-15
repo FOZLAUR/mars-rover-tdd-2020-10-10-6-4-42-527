b504f6485026835271b5bc8626b67e5219b65f36
FOZLAUR <fozla@oocl.com>
2020-10-15 21:25:54 +0800

    [mars-rover-tdd][LaurenceFoz] test: Added test case for (0,0) N MLMR; No changes to logic

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -186,4 +186,19 @@ public class DemoTest {
         assertEquals(0,marsRover.getLocationY());
         assertEquals("E",marsRover.getHeading());
     }
+
+    @Test
+    void should_return_x_negative_1_y_1_heading_N_when_MLMR_given_x_0_y_0_heading_N() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        List<String> commands = new CommandSplitter("MLMR").getCommandList();
+
+        //when
+        marsRover.executeBatchCommand(commands);
+
+        //should
+        assertEquals(-1,marsRover.getLocationX());
+        assertEquals(1,marsRover.getLocationY());
+        assertEquals("N",marsRover.getHeading());
+    }
 }
