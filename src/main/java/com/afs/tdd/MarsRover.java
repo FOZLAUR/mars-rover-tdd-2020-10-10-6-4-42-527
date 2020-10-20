c538bf82a5918009024f3f3666a87337229a565b
FOZLAUR <fozla@oocl.com>
2020-10-20 21:03:47 +0800

    [mars-rover-tdd][LaurenceFoz] refactor: Removed unused methods and function in MarsRover class

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MarsRover {
-    final private static List<String> HEADINGS = Arrays.asList("N","E","S","W");
     private RoverStatus roverStatus;
     private CommandInvoker commandInvoker = new CommandInvoker();
 
@@ -28,33 +27,6 @@ public class MarsRover {
             default : System.out.println("Empty command");
         }
     }
-
-//    private void changeHeading(String direction) {
-//        int newHeading = -1;
-//        String currentHeading = roverStatus.getCurrentHeading();
-//        switch(direction){
-//            case "L":
-//                newHeading = (HEADINGS.indexOf(currentHeading) > 0) ? HEADINGS.indexOf(currentHeading)-1 : 3  ;
-//                roverStatus.setCurrentHeading(HEADINGS.get(newHeading));
-//                break;
-//            case "R" :
-//                newHeading = (HEADINGS.indexOf(currentHeading) < 3) ? HEADINGS.indexOf(currentHeading)+1 : 0;
-//                roverStatus.setCurrentHeading(HEADINGS.get(newHeading));
-//                break;
-//            default:
-//        }
-//    }
-//
-//    private void moveRover() {
-//        String currentHeading = roverStatus.getCurrentHeading();
-//        switch(currentHeading){
-//            case "N": roverStatus.incrementY(); break;
-//            case "S": roverStatus.decrementY();; break;
-//            case "E": roverStatus.incrementX(); break;
-//            case "W": roverStatus.decrementX();; break;
-//            default :
-//        }
-//    }
 
     public int getLocationX() {
         return roverStatus.getX();
