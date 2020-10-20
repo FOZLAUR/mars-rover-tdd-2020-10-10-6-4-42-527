b2c2ee01f0bdba64f76ae6326ad2a55b581509c1
FOZLAUR <fozla@oocl.com>
2020-10-20 20:35:17 +0800

    [mars-rover-tdd][LaurenceFoz] feature: Added CommandExecutor which does the business logic

---
+++ src/main/java/com/afs/tdd/CommandExecutor.java
@@ -0,0 +1,38 @@
+package com.afs.tdd;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CommandExecutor {
+    final private static List<String> HEADINGS = Arrays.asList("N","E","S","W");
+    private RoverStatus roverStatus;
+
+    public CommandExecutor(RoverStatus roverStatus){
+        this.roverStatus = roverStatus;
+    }
+
+    public void turnLeft() {
+        int newHeading = -1;
+        String currentHeading = roverStatus.getCurrentHeading();
+        newHeading = (HEADINGS.indexOf(currentHeading) > 0) ? HEADINGS.indexOf(currentHeading)-1 : 3  ;
+        roverStatus.setCurrentHeading(HEADINGS.get(newHeading));
+    }
+
+    public void turnRight() {
+        int newHeading = -1;
+        String currentHeading = roverStatus.getCurrentHeading();
+        newHeading = (HEADINGS.indexOf(currentHeading) < 3) ? HEADINGS.indexOf(currentHeading)+1 : 0;
+        roverStatus.setCurrentHeading(HEADINGS.get(newHeading));
+    }
+
+    public void moveRover() {
+        String currentHeading = roverStatus.getCurrentHeading();
+        switch(currentHeading){
+            case "N": roverStatus.incrementY(); break;
+            case "S": roverStatus.decrementY();; break;
+            case "E": roverStatus.incrementX(); break;
+            case "W": roverStatus.decrementX();; break;
+            default :
+        }
+    }
+}
